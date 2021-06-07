4d680aaf838c20b7609c23ac3112257b9b7ecda8
Gabriele Quevedo <54038952+gsquevedo@users.noreply.github.com>
2021-06-07 10:59:37 -0300

    Update Main.hs

--- Main.hs
+++ Main.hs
@@ -1,1 +1,24 @@
+module Main where
+import FormaFigura 
+import Text.Printf
 
+main :: IO ()
+main = do
+  writeFile "figura.svg" $ figura
+  where figura = svgBegin w h ++ svgCirculo ++ svgRetangulo ++ svgStar ++ svgEnd
+        svgCirculo = svgElements svgCircle circulo (map svgStyleCirculo cores)
+        circulo = genCirclesInLine ncirculo
+        cores = rgbCirculo ncirculo
+        ncirculo = 1
+
+        svgRetangulo = svgElements svgRect rect (map svgStyleRetangulo palette)
+        rect = genRectsInLine nreact
+        palette = rgbRetangulo nreact
+        nreact = 1
+
+        svgStar= svgElements svgEstrela estrela (map svgStyleEstrela corEstrela)
+        estrela = genEstrelaInLine nestrela
+        corEstrela = rgbEstrela nestrela
+        nestrela = 1
+
+        (w,h) = (800,900)
