8710bd7e031db067785be0803ade88fe22dd53e5
Gabriele Quevedo <54038952+gsquevedo@users.noreply.github.com>
2021-06-14 12:24:05 -0300

    Update Main.hs

--- Main.hs
+++ Main.hs
@@ -5,67 +5,67 @@ type Rect      = (Point,Float,Float)
 type Circle    = (Point,Float)
 type Triangle  = (Point,Point,Point)
 
-------------------------------------------------------------------------- Informações da imagem 
------------------------------------------------------------------------
+---------------------------------------------------------------------- Informações da imagem 
+--------------------------------------------------------------------
 maxLargura :: Float
 maxLargura = 800
 
 maxAltura :: Float
 maxAltura = 500
 
-------------------------------------------------------------------------- Informaçoes das figuras
------------------------------------------------------------------------
+----------------------------------------------------------------------- Informaçoes das figuras
+--------------------------------------------------------------------
 maxRaio :: Float
-maxRaio = 20
-
-maxLado :: Float
-maxLado = 50
+maxRaio = 15
 
 numTriangulos :: Int
 numTriangulos = 10
 
 numCirculos :: Int
-numCirculos = 3
+numCirculos = 5
 
 numRetangulo :: Int
 numRetangulo = 5
 
-----------------------------------------------------------------------
+maxLado :: Int
+maxLado = 50
+
+--------------------------------------------------------------------
 -- Paletas de cores
------------------------------------------------------------------------
+--------------------------------------------------------------------
 rgbRetangulo :: Int -> [(Int,Int,Int)]
-rgbRetangulo n = [(i*a,i,i-a) | i <- take n(iterate(+b)a)]
-  where a = 10
-        b = 20
+rgbRetangulo n = [(i*a,a,i*a) | i <- take n(iterate(+b)a)]
+  where a = maxLado 
+        b = 10
 
 rgbCirculo :: Int -> [(Int,Int,Int)]
 rgbCirculo n = [(i*a, a+i, i-a) | i <- take n(iterate(+15)a)]
-  where a = raioCirc maxRaio
+  where a = corCirc maxRaio
 
 rgbTriangulo :: Int -> [(Int,Int,Int)]
 rgbTriangulo n = [(i*a,a*b,i+a) | i <- take n(iterate(+b)a)]
   where a = corTri numTriangulos
         b = 10
---------------------------------------------------------------------------- Funções utilizadas para formar as figuras
------------------------------------------------------------------------
 
---muda o tipo de imagem do retangulo conforme o lado
-tipoLado :: Float -> String
-tipoLado maxLado 
-    |(maxLado >= 50  && maxLado < 100) = "difference"
-    |(maxLado >= 100 && maxLado < 150) = "exclusion"
-    |otherwise = "screen"
+--------------------------------------------------------------------------- Funções utilizadas para formar as figuras
+--------------------------------------------------------------------
 
 --retorna um raio a cada valor
 raioAleatorio :: Float -> Float
 raioAleatorio r = 2*pi+maxRaio
 
+--convert valores int para float
+convTri :: Float
+convTri = fromIntegral numTriangulos
+
+convLado :: Float
+convLado = fromIntegral maxLado
+
 --funçao muda a cor do circulo
-raioCirc :: Float -> Int
-raioCirc maxRaio
+corCirc :: Float -> Int
+corCirc maxRaio
   |(maxRaio >= 5 && maxRaio < 10) = 0
   |(maxRaio >= 10 && maxRaio < 15) = 5
-  |(maxRaio >= 15 && maxRaio < 20) = 15
   |otherwise = 50
 
 corTri :: Int -> Int
@@ -75,22 +75,22 @@ corTri numTriangulos
   |(numTriangulos > 10 && numTriangulos <= 15) = 15
   |otherwise = 20
  
-----------------------------------------------------------------------
+--------------------------------------------------------------------
 -- Geração de circulos  em suas posições
-----------------------------------------------------------------------
+--------------------------------------------------------------------
 genRetanguloInLine :: Int -> [Rect]
-genRetanguloInLine n  = [((maxLargura-maxLado*m, 100*m), maxLargura, maxAltura) | m <- [0..fromIntegral (n-1)]]
+genRetanguloInLine n  = [((maxLargura-convLado*m-100, 100*m), maxLargura, maxAltura) | m <- [0..fromIntegral (n-1)]]
 
 genCircleInLine :: Int -> [Circle] 
 genCircleInLine n = [((100,y),raioAleatorio maxRaio*m) | m <- [1..fromIntegral (n)]]
   where y = 150
 
 genTrianguloInLine :: Int -> [Triangle]
-genTrianguloInLine n = [((maxAltura/2,m*maxAltura/fromIntegral numTriangulos),(maxAltura,maxAltura/2),(350,maxAltura/2)) | m <- [0..fromIntegral(n-1)]]
+genTrianguloInLine n = [((maxAltura/2,m*maxAltura/convTri),(maxAltura,maxAltura/2),(maxAltura/2+100,maxAltura/2)) | m <- [0..fromIntegral(n-1)]]
 
-----------------------------------------------------------------------
+--------------------------------------------------------------------
 -- Strings SVG
-----------------------------------------------------------------------
+--------------------------------------------------------------------
 svgRetangulo :: Rect -> String -> String 
 svgRetangulo ((x,y),w,h) style = 
   printf "<rect x='%.3f' y='%.3f' width='%.2f' height='%.2f' style='%s' />\n" x y w h style
@@ -103,37 +103,36 @@ svgTriangulo :: Triangle -> String -> String
 svgTriangulo ((x1,y1),(x2,y2),(x3,y3)) style =
   printf "<polygon points = '%.3f,%.3f %.3f,%.3f %.3f,%.3f' style='%s' />\n" x1 y1 x2 y2 x3 y3 style
 
--------------------------------------------------------------------------- String inicial do SVG
-----------------------------------------------------------------------
+------------------------------------------------------------------------ String inicial do SVG
+--------------------------------------------------------------------
 svgBegin :: Float -> Float -> String
 svgBegin w h = printf "<svg width='%.2f' height='%.2f' xmlns='http://www.w3.org/2000/svg'>\n" w h 
 
--------------------------------------------------------------------------- String final do SVG
-----------------------------------------------------------------------
+------------------------------------------------------------------------ String final do SVG
+--------------------------------------------------------------------
 svgEnd :: String
 svgEnd = "</svg>"
 
-----------------------------------------------------------------------
+--------------------------------------------------------------------
 -- Gera string com atributos de estilo para uma dada cor
-----------------------------------------------------------------------
+--------------------------------------------------------------------
 svgStyleTriangulo :: (Int,Int,Int) -> String
 svgStyleTriangulo (r,g,b) = "fill:rgb"++show(r,g,b)++";"++"stroke-width:5;stroke:rgb(0,0,0);"
 
 svgStyleRetangulo :: (Int,Int,Int) -> String
-svgStyleRetangulo (r,g,b) = "fill:rgb"++show(r,g,b)++";"++"mix-blend-mode:"++tipoLado maxLado++";"
+svgStyleRetangulo (r,g,b) = "fill:rgb"++show(r,g,b)++";"++"mix-blend-mode:difference;stroke:rgb(0,0,0);stroke-width:3;"
 
 svgStyleCirculo :: (Int,Int,Int) -> String
 svgStyleCirculo (r,g,b) = "fill:rgb"++show(r,g,b)++";"++"mix-blend-mode:screen;"
 
-----------------------------------------------------------------------
+--------------------------------------------------------------------
 -- Gera strings SVG para uma dada lista de figuras e seus atributos de estilo
-----------------------------------------------------------------------
+--------------------------------------------------------------------
 svgElements :: (a -> String -> String) -> [a] -> [String] -> String
 svgElements func elements styles = concat $ zipWith func elements styles
 
-
--------------------------------------------------------------------------- FUNÇÃO PRINCIPAL
-----------------------------------------------------------------------
+---------------------------------------------------------------------- FUNÇÃO PRINCIPAL
+--------------------------------------------------------------------
 main :: IO ()
 main = do
   writeFile "figura.svg" $ svgstrs
