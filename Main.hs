113b511e0abcaaa2600a8fd2c5f3541a6a7312ee
Gabriele Quevedo <54038952+gsquevedo@users.noreply.github.com>
2021-06-15 12:01:57 -0300

    Add files via upload

--- Main.hs
+++ Main.hs
@@ -1,3 +1,4 @@
+
 import Text.Printf
 
 type Point     = (Float,Float)
@@ -6,90 +7,61 @@ type Circle    = (Point,Float)
 type Triangle  = (Point,Point,Point)
 
 --------------------------------------------------------------------
--- Informações da imagem 
---------------------------------------------------------------------
-maxLargura :: Float
-maxLargura = 800
-
-maxAltura :: Float
-maxAltura = 500
-
----------------------------------------------------------------------
--- Informaçoes das figuras
---------------------------------------------------------------------
-maxRaio :: Float
-maxRaio = 10
-
-numTriangulos :: Int
-numTriangulos = 10
-
-numCirculos :: Int
-numCirculos = 5
-
-numRetangulo :: Int
-numRetangulo = 5
-
-maxLado :: Int
-maxLado = 50
-
---------------------------------------------------------------------
 -- Paletas de cores
 --------------------------------------------------------------------
 rgbRetangulo :: Int -> [(Int,Int,Int)]
-rgbRetangulo n = [(i*a,a,i*a) | i <- take n(iterate(+b)a)]
-  where a = maxLado 
-        b = 10
+rgbRetangulo n = [(0,0,40*i) | i <- [1..n]]
 
-rgbCirculo :: Int -> [(Int,Int,Int)]
-rgbCirculo n = [(i*a, a+i, i-a) | i <- take n(iterate(+15)a)]
-  where a = corCirc maxRaio
-
+rgbCirculo :: Int -> Int -> [(Int,Int,Int)]
+rgbCirculo n raio = [(i*a,a*10,i+a) | i <- take n(iterate(+10)a)]
+  where a = corCirc raio
+      
 rgbTriangulo :: Int -> [(Int,Int,Int)]
 rgbTriangulo n = [(i*a,a*b,i+a) | i <- take n(iterate(+b)a)]
-  where a = corTri numTriangulos
+  where a = corTri n
         b = 10
 
-------------------------------------------------------------------------
+--------------------------------------------------------------------
 --- Funções utilizadas para formar as figuras
 --------------------------------------------------------------------
-
 --retorna um raio a cada valor
 raioAleatorio :: Float -> Float
-raioAleatorio r = 2*pi+maxRaio
-
---convert valores int para float
-convTri :: Float
-convTri = fromIntegral numTriangulos
-
-convLado :: Float
-convLado = fromIntegral maxLado
+raioAleatorio r = 2*pi+r
 
 --funçao muda a cor do circulo
-corCirc :: Float -> Int
+corCirc :: Int -> Int
 corCirc maxRaio
-  |(maxRaio >= 5 && maxRaio < 10) = 0
-  |(maxRaio >= 10 && maxRaio < 15) = 5
-  |otherwise = 50
+  |(maxRaio >= 5 && maxRaio < 10) = 5 
+  |(maxRaio >= 10 && maxRaio < 15) = 15 
+  |(maxRaio >= 15 && maxRaio < 20) = 5
+  |otherwise = 0 
 
 corTri :: Int -> Int
 corTri numTriangulos 
-  |(numTriangulos >= 1 && numTriangulos <= 5) = 5
-  |(numTriangulos > 5 && numTriangulos <= 10) = 10
-  |(numTriangulos > 10 && numTriangulos <= 15) = 15
-  |otherwise = 20
- 
+  |(numTriangulos >= 1 && numTriangulos <= 5) = 3
+  |(numTriangulos > 5 && numTriangulos <= 10) = 15
+  |(numTriangulos > 10 && numTriangulos <= 15) = 5
+  |otherwise = 10
+
+
 --------------------------------------------------------------------
 -- Geração de circulos  em suas posições
 --------------------------------------------------------------------
-genRetanguloInLine :: Int -> [Rect]
-genRetanguloInLine n  = [((maxLargura-convLado*m-100, 100*m), maxLargura, maxAltura) | m <- [0..fromIntegral (n-1)]]
 
-genCircleInLine :: Int -> [Circle] 
-genCircleInLine n = [((100,y), raioAleatorio maxRaio*m) | m <- [1..fromIntegral (n)]]
-  where y = 150
+genRetanguloInLine :: Int -> Float -> [Rect]
+genRetanguloInLine n largura = [((m*(w+gap), (largura/2)), w, h) | m <-  [0..fromIntegral (n-1)]]
+  where (w,h) = (50,50)
+        gap = 10
 
-genTrianguloInLine :: Int -> [Triangle]
-genTrianguloInLine n = [((maxAltura/2, m*maxAltura/convTri),(maxAltura, maxAltura/2),(maxAltura/2+100, maxAltura/2)) | m <- [0..fromIntegral(n-1)]]
+genRectInLine :: Int -> Float -> Float -> Float ->  [Rect]
+genRectInLine n largura w h = [((largura-100,m*(w+gap)),w,h) | m <- [0..fromIntegral(n-1)]]
+  where gap = 10
+
+genCircleInLine :: Int -> Int -> (Float,Float) -> [Circle] 
+genCircleInLine n raio (x,y) = [((x,y), raioAleatorio (fromIntegral raio)*m) | m <- [1..fromIntegral (n)]]
+
+genTrianguloInLine :: Int -> Float -> Int -> [Triangle]
+genTrianguloInLine n altura num = [((altura/2,m*altura/(fromIntegral num)),(altura, altura/2),(altura/2+100, altura/2)) | m <- [0..fromIntegral(n-1)]]
 
 --------------------------------------------------------------------
 -- Strings SVG
@@ -112,7 +84,7 @@ svgTriangulo ((x1,y1),(x2,y2),(x3,y3)) style =
 svgBegin :: Float -> Float -> String
 svgBegin w h = printf "<svg width='%.2f' height='%.2f' xmlns='http://www.w3.org/2000/svg'>\n" w h 
 
-----------------------------------------------------------------------
+--------------------------------------------------------------------
 -- String final do SVG
 --------------------------------------------------------------------
 svgEnd :: String
@@ -125,7 +97,7 @@ svgStyleTriangulo :: (Int,Int,Int) -> String
 svgStyleTriangulo (r,g,b) = "fill:rgb"++show(r,g,b)++";"++"stroke-width:5;stroke:rgb(0,0,0);"
 
 svgStyleRetangulo :: (Int,Int,Int) -> String
-svgStyleRetangulo (r,g,b) = "fill:rgb"++show(r,g,b)++";"++"mix-blend-mode:difference;stroke:rgb(0,0,0);stroke-width:3;"
+svgStyleRetangulo (r,g,b) = "fill:rgb"++show(r,g,b)++";"++"mix-blend-mode:normal;"
 
 svgStyleCirculo :: (Int,Int,Int) -> String
 svgStyleCirculo (r,g,b) = "fill:rgb"++show(r,g,b)++";"++"mix-blend-mode:screen;"
@@ -141,21 +113,43 @@ svgElements func elements styles = concat $ zipWith func elements styles
 --------------------------------------------------------------------
 main :: IO ()
 main = do
-  writeFile "figura.svg" $ svgstrs
-  where svgstrs = svgBegin w h  ++ svgTriangle ++ svgCircle ++ svgRect ++ svgEnd  
+    putStrLn "Informe a largura e a altura da imagem:"
+    largura <- getLine
+    altura <- getLine
+    putStrLn "Informe o número de circulos:"
+    numCirculos <- getLine
+    putStrLn "Informe o número de triângulos:"
+    numTriangulos <- getLine
+    putStrLn "Informe  o número de retângulos:"
+    numRetangulo <- getLine 
+    putStrLn "Informe o raio do círculo:"
+    raio <- getLine
+    putStrLn "Informe a posicao (x,y) do circulo:"
+    x <- getLine 
+    y <- getLine
+    putStrLn "Informe  a altura e largura do retângulo:"
+    alt <- getLine 
+    larg <- getLine
+    let svgstrs = svgBegin w h  ++ svgTriangle ++ svgCircle ++ svgRect ++ svgReact ++ svgEnd  
         svgTriangle = svgElements svgTriangulo triangulo (map svgStyleTriangulo corTriangulo)
-        triangulo = genTrianguloInLine ntriangulo
+        triangulo = genTrianguloInLine ntriangulo h ntriangulo
         corTriangulo = rgbTriangulo ntriangulo
-        ntriangulo = numTriangulos
+        ntriangulo = read numTriangulos
 
         svgCircle = svgElements svgCirculo circle (map svgStyleCirculo corCircle)
-        circle = genCircleInLine ncircle
-        corCircle = rgbCirculo ncircle
-        ncircle = numCirculos
+        circle = genCircleInLine ncircle (read raio) (read x,read y)
+        corCircle = rgbCirculo ncircle (read raio)
+        ncircle = read numCirculos
 
         svgRect = svgElements svgRetangulo retangulo (map svgStyleRetangulo corRetangulo)
-        retangulo = genRetanguloInLine nretangulo
-        corRetangulo = rgbRetangulo nretangulo
-        nretangulo = numRetangulo
+        retangulo = genRetanguloInLine nretangulo w
+        corRetangulo = rgbRetangulo nretangulo 
+        nretangulo = read numRetangulo
 
-        (w,h) = (maxLargura,maxAltura) -- largura e altura da imagem SVG 
+        svgReact = svgElements svgRetangulo rect (map svgStyleRetangulo corRect)
+        rect = genRectInLine nrect w (read alt) (read larg)
+        corRect = rgbRetangulo nrect 
+        nrect = read numRetangulo
+
+        (w,h) = (read largura,read altura) -- largura e altura da imagem SVG 
+      in writeFile "figura.svg" $ svgstrs  
--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 
 Generative Art com Programação Funcional em Haskell
+Nome: Gabriele Soares Quevedo 
+Curso: Ciência da Computação 
 
 Como compilar:
 Para compilar o arquivo utilizei os seguintes comandos
@@ -8,10 +10,16 @@ Para compilar o arquivo utilizei os seguintes comandos
 - main 
  
 Como funciona: 
-Fiz algumas alterações simples no Generative Art, a princípio a ideia foi desenhar círculos, retângulo e triângulos que serão controlados pelas coordenadas que o usuário modificar no arquivo. Pelo raio, lado e quantidade de figuras. 
-- O círculo são interligados pelo tamanho do raio. Tornei o 'x' do círculo estático (apenas por opção), para modificar a posição do círculo deixei o 'y' em aberto, ou seja,o círculo será desenhado apenas na coordenada y. Os círculos são desenhados um dentro do outro, pois a função genCircleInLine, modifica o raio a cada interação da list, também uso uma função para calcular o raio do círculo utilizando a constante Pi. Para mudar a cor do círculo, fiz a função rgbCirculo, deixando uma variável 'a' em aberto, que é utilizada para modificar a cor conforme o raio, nessa variável chamo a função(raioCirc). Obs: Utilizei o tipo "screen" no style do circulo para deixar um efeito diferente. Uma maneira de conferir que o circulo está sendo desenhado um em cima do outro.
-- Os retângulos são interligados pelas coordenadas, utilizo uma função(maxLado) para mudar a cor do retângulo, que ao ir aumentando esse valor retorna um retângulo com tons de rosa e também será desenhado retângulos maiores. Obs: Utilizei o tipo "difference" no style do retângulo para deixar um efeito. É desenhado conforme a largura e altura da imagem (apenas por opção, já que a modificação acontece em relação ao tamanho do lado e a cor), isto é, torna-se uma figura estática, ao modificar essas coordenadas, a figura continuará na mesma posição.
-- Os triângulos são desenhado sobreposto sobre sua coordenada. Suas cores variam conforme aumenta o número de triângulos ou se modificar a variável 'b' em sua função(rgbTriangulo), por exemplo b = 5, retorna tons de vermelho. É desenhado conforme a largura e altura da imagem (apenas por opção, já que a modificação acontece em relação a cor), isto é, torna-se uma figura estática, ao modificar essas coordenadas, a figura continuará na mesma posição, mas com imagem de tamanho diferente.
+Deve-se informar as informações pedidas pelo programa (altura e largura da imagem, quantidade de figuras, raio e posição do círculo)
+Fiz algumas alterações simples no Generative Art, a princípio a ideia foi desenhar círculos, retângulo e triângulos que serão controlados pelas coordenadas que o usuário informar pela linha de comando(raio e quantidade de figuras) ou no arquivo(a altura e largura do retângulo).
+
+- O círculo são interligados pelo tamanho do raio. Tornei a posição do círculo dinâmica, o usuário digita a posição que desejar. Os círculos são desenhados um dentro do outro, pois a função genCircleInLine, modifica o raio a cada interação da list, também uso uma função para calcular o raio do círculo utilizando a constante Pi. Para mudar a cor do círculo, fiz a função rgbCirculo, deixando uma variável 'a' em aberto, que é utilizada para modificar a cor conforme o raio, nessa variável chamo a função(corCirc). Obs: Utilizei o tipo "screen" no style do circulo para deixar um efeito diferente. Uma maneira de conferir que o circulo está sendo desenhado um em cima do outro com raios diferentes.
+
+- Os retângulos são interligados pelas coordenadas, optei por desenhar retângulo azuis porém em posições diferentes (vertical e horizontal).  Ambos tem tons degradê de azul, ao aumentar a quantidade de figuras. A imagem também é estática. Porém na imagem vertical, o usuário digita a largura e altura do retângulo na linha de comando, na outra pode ser modificado pelo arquivo.
+
+- Os triângulos são desenhado sobreposto sobre sua coordenada. Suas cores variam conforme aumenta o número de triângulos. É desenhado conforme a largura e altura da imagem (apenas por opção, já que a modificação acontece em relação a cor), isto é, torna-se uma figura estática, a figura continuará na mesma posição, mas com imagem de tamanho diferente.
+
 ![img](/t1.svg)
 ![img](/t2.svg)
 ![img](/t3.svg)
+![img](/t4.svg)
